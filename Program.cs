6e9ef7d879611a5b1eb0a9fef0a00aeae6917f59
Asha Krishna S <68347359+AshaKrishnaS@users.noreply.github.com>
2021-11-20 19:49:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace constructorprograms
+{
+    class Program
+    {
+        public string model;
+        public Program()
+        {
+
+            model = "Mustang";    
+           }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.model);
+
+        }
+    }
+}
